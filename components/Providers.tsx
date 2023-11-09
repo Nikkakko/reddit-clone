272ebfe83b2c4f74c607adf6bbead4ff14d331a5
Nika Kopadze <nikolozkopadze@gmail.com>
2023-11-09 16:25:21 +0400

    initilize folder, added prisma models

---
+++ components/Providers.tsx
@@ -0,0 +1,18 @@
+'use client';
+import * as React from 'react';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import { type ThemeProviderProps } from 'next-themes/dist/types';
+
+import { TooltipProvider } from '@/components/ui/tooltip';
+import { Toaster } from '@/components/ui/toaster';
+
+export function Providers({ children, ...props }: ThemeProviderProps) {
+  return (
+    <NextThemesProvider {...props}>
+      <TooltipProvider>
+        {children}
+        <Toaster />
+      </TooltipProvider>
+    </NextThemesProvider>
+  );
+}
---
+++ middleware.ts
@@ -0,0 +1,26 @@
+import { authMiddleware } from '@clerk/nextjs';
+import { NextResponse } from 'next/server';
+
+// This example protects all routes including api/trpc routes
+// Please edit this to allow other routes to be public as needed.
+// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
+export default authMiddleware({
+  publicRoutes: ['/'],
+
+  async afterAuth(auth, req) {
+    if (auth.isPublicRoute) {
+      return NextResponse.next();
+    }
+
+    const url = new URL(req.nextUrl.origin);
+
+    if (!auth.userId) {
+      url.pathname = '/signin';
+      return NextResponse.redirect(url);
+    }
+  },
+});
+
+export const config = {
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+};
